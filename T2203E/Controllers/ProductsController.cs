3030d089ab242a1e22c09d9e602756c7bf2e6c2e
macbook <quanghoa.trinh@gmail.com>
2023-05-04 21:15:53 +0700

    crud

---
+++ T2203E/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using T2203E.Entities;
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace T2203E.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            List<Product> list =  _context.Products.ToList<Product>();
+            ViewBag.products = list;
+            return View();
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var product = await _context.Products.FindAsync(Id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
+
