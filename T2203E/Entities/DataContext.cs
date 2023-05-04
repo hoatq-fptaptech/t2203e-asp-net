fdd16c9ef131bb96258ac053fdbf14c22a292fbb
macbook <quanghoa.trinh@gmail.com>
2023-05-04 19:01:05 +0700

    first commit

---
+++ T2203E/Entities/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace T2203E.Entities
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options): base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
+
---
+++ T2203E/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace T2203E.Entities
+{
+	[Table("Products")]
+	public class Product
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(200)]
+		public string Name { get; set; }
+
+		[Required]
+		public double Price { get; set; }
+
+		[StringLength(150)]
+		public string Thumbnail { get; set; }
+	}
+}
+
