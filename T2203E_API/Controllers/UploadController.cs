a8bab36fc27aa5185ccd7f80b0a324f365bd74a9
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-06-16 19:31:41 +0700

    upload

---
+++ T2203E_API/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace T2203E_API.Controllers
+{
+    [ApiController]
+    [Route("/api/upload")]
+    public class UploadController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("image")]
+        public IActionResult Index(IFormFile image)
+        {
+            if(image == null)
+            {
+                return BadRequest("Vui lòng gửi file đính kèm");
+            }
+            var path = "wwwroot/uploads";
+            var fileName = Guid.NewGuid().ToString()+ Path.GetFileName(image.FileName);
+            var upload = Path.Combine(Directory.GetCurrentDirectory(), path, fileName);
+            image.CopyTo(new FileStream(upload, FileMode.Create));
+            var rs = $"{Request.Scheme}://{Request.Host}/uploads/{fileName}";
+            return Ok(rs);
+        }
+    }
+}
+
--- T2203E_API/Program.cs
+++ T2203E_API/Program.cs
@@ -52,6 +52,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseCors();
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 
